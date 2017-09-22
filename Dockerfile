1f9acdfd41a8aca599b9ca880d09961c8ecdb378
gayanj <gayan@bluedrop.com>
2017-09-22 00:28:05 -0300

    working version

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,14 @@ FROM alpine
 ENV KUBE_VERSION="v1.7.2"
 ENV HELM_VERSION="v2.6.1"
 
-RUN apk add --update ca-certificates \
- && apk add --update -t deps curl git \
+RUN  apk add --no-cache curl git \
  && curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBE_VERSION}/bin/linux/amd64/kubectl \
  && chmod +x ./kubectl \
  && mv ./kubectl /usr/local/bin/kubectl \
  && curl -LO https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz \
  && tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz \
  && mv linux-amd64/helm /usr/local/bin/helm \
- && rm -rf linux-amd64 helm-${HELM_VERSION}-linux-amd64.tgz  \
- && apk del --purge deps \
- && rm /var/cache/apk/*
+ && rm -rf linux-amd64 helm-${HELM_VERSION}-linux-amd64.tgz
 
 COPY deploy.sh /
 
--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,12 @@ fi
 
 # Extract cluster context and set to current-context
 if [ ! -z "$CLUSTER_CONTEXT" ]; then
-	kubectl config use-context CLUSTER_CONTEXT
+	kubectl config use-context ${CLUSTER_CONTEXT}
 	echo "Helm is using ${CLUSTER_CONTEXT} cluster context"
 fi
 
 # Extract the bitbucket project url
 if [[ ! -z "$BITBUCKET_PROJECT_CLONE_URL" ]]; then
-	BITBUCKET_PROJECT_CLONE_URL="$(base64 -d <<< $BITBUCKET_PROJECT_CLONE_URL)"
 	echo "Bitbucket project is downloading..."
 	git clone ${BITBUCKET_PROJECT_CLONE_URL}
 	echo "Downloaded successful."
@@ -26,8 +25,8 @@ fi
 
 # Run helm command
 if [[ ! -z "$HELM_COMMAND" ]]; then
-	echo "Starting to run ${HELM_COMMAND}"
-	HELM_COMMAND
+	echo "Running ${HELM_COMMAND}"
+	${HELM_COMMAND}
 else
 	echo "Helm command not found. Please provide a helm command"
 fi
